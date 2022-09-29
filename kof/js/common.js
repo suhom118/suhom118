6323cd68175f3cb96095a32948a7eef2c55d16c7
suhom118 <suhom118@gmail.com>
2022-09-29 16:12:00 +0900

    tert

--- kof/js/common.js
+++ kof/js/common.js
@@ -1,56 +1,71 @@
+// header footer에 포함되는jquery
 $(document).ready(function(){
-
-
-    if(winW > 640){
-        pcMobile = 'pc';
-        console.log(pcMobile);
+    let pcMo//pc일때 pc 모바일일때는 모바일
+    let winW
+    pcChk
+    
+    if(winW>640){
+        pcMo='pc'
     }else{
-        pcMobile = 'mobile';
-        console.log(pcMobile);
+        pcMo='Mobile'
     }
 
+    // console.log(pcMo)
+
     $(window).resize(function(){
-        winW = $(window).width();
-        if(winW > 640){
-            pcMobile = 'pc';
-            console.log(pcMobile);
+        pcChk()
+        
+    })//window.resize종료
+
+    function pcChk(){
+        winW=$(window).width()
+        if(winW>640){
+            pcMo='pc'
         }else{
-            pcMobile = 'mobile';
-            console.log(pcMobile);
+            pcMo='Mobile'
         }
-    });
-
-    /*
-        .header .lang .open 클릭했을 때
-        1. ul이 나타나야함
-        2. open 숨김, close 나타야함
-        --> .header .lang 에 lang_open 클래스를 추가
-
-        .header .lang .close 클릭하면
-        --> .header .lang 에 lang_open 클래스를 삭제
-    */
-    $('.header .lang .open').on('click', function(){
-        $('.header .lang').addClass('lang_open');
-    });
-    $('.header .lang .close').on('click', function(){
-        $('.header .lang').removeClass('lang_open');
-    });
-
-    /* 
-        메뉴 (.header .gnb > ul > li > a)에 마우스를 오버했을때 
-        header에  menu_open 클래스 추가
-
-        메뉴 (.header)에 마우스를 아웃했을때 (벗어났을때)
-        header에 menu_open 클래스 삭제
-    */
-    $('.header .gnb > ul > li > a').on('mouseenter focusin', function(){
-        if(pcMobile == 'pc'){
-            $('.header').addClass('menu_open');
+        // console.log(pcMo)
+    }
+    $('.header .gnb>ul>li').on('mouseenter focusin', function(){
+        if(pcMo=='pc'){
+            $('.header').addClass('menu_open')
         }
-    });
+    })
     $('.header').on('mouseleave', function(){
-        $('.header').removeClass('menu_open');
+        $('.header').removeClass('menu_open')
+    })
+    $('.header .gnb>ul>li:last-child>ul>li:last-child>a').on('focusout', function(){
+        $('.header').removeClass('menu_open')
+    })
+
+
+
+    $(document).ready(function(){
+        let scrolling = $(window).scrollTop();
+        $(window).scroll(function(){
+            scrolling = $(window).scrollTop();
+            console.log(scrolling);
+            if(scrolling > 0){
+               $('header').addClass('fixed'); 
+            }else{
+                $('header').removeClass('fixed');
+            }
+        });
     });
-    $('.header .gnb > ul > li:last-child > ul > li:last-child > a').on('focusout', function(){
-        $('.header').removeClass('menu_open');
-    });
+    $('.header .gnb .gnb_open').on ('click', function(){
+        $('.header').addClass('menu_mobile')
+    })
+    $('.header .gnb .gnb_close').on ('click', function(){
+        $('.header').removeClass('menu_mobile')
+    })
+
+
+
+    $('.header .gnb>ul>li>a').on('click', function(e){
+        if(pcMo == 'Mobile'){
+            e.preventDefault()
+            $(this).parents('li').toggleClass('sub_open')
+        }
+    })
+
+})//document.ready종료
