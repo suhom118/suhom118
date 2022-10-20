cca74db7398b76a9a6c2f8eed2c545c2bb9df58c
suhom118 <suhom118@gmail.com>
2022-10-20 09:29:26 +0900

    sdfs

--- savethechildren/js/main.js
+++ savethechildren/js/main.js
@@ -1,26 +1,31 @@
-$(document).ready(function () {
-  const myFullpage = new fullpage("#fullpage", {
-    /* html에서 페이지 전체를 감싸는 요소 */
+$(document).ready(function(){
 
-    navigation: true /* 오른쪽에 각 페이지의 paging */,
-    navigationPosition: "right" /* 위치 */,
-    navigationTooltips: ["메인", "보건통계", "사업소개", "현장소식", "지원사업"] /* 툴팁 */,
-    showActiveTooltip: true /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */,
+    // const myFullpage = new fullpage('#fullpage', {});  이렇게만 써도 fullpage는 기본 동작함
 
-    autoScrolling: true /* 한페이지씩 스크롤 */,
-    scrollHorizontally: true,
+    const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
 
-    verticalCentered: true /* 컨텐츠 요소 위아래 가운데 */,
+		navigation: true, /* 오른쪽에 각 페이지의 paging */
+		navigationPosition: 'right', /* 위치 */
+		navigationTooltips: ['메인', '보건통계', '사업소개', '현장소식', '지원사업'], /* 툴팁 */
+		showActiveTooltip: false, /* 현재 활성화된 페이지의 툴팁 보이기 */
 
-    responsiveWidth: 640 /* fullpage를 적용시키지 않을 모바일 사이즈 */,
+		afterLoad: function(origin, destination, direction, trigger){
+			//console.log(destination.index);
+			if((destination.index == 2)||(destination.index == 3)||(destination.index == 4)||(destination.index == 5)){
+				$('.header').addClass('black');
+				$('#fp-nav').addClass('black');
+			}else{
+				$('.header').removeClass('black');
+				$('#fp-nav').removeClass('black');
+			}
+		},
+		responsiveWidth: 640 /* fullpage를 적용시키지 않을 모바일 사이즈 */
+	});//fullpage
 
-    onLeave: function (origin, destination, direction, trigger) {},
-    beforeLeave: function (origin, destination, direction, trigger) {
-      if (destination.index >= 2) {
-        $(".header").addClass("black");
-      } else {
-        $(".header").removeClass("black");
-      }
-    },
-  });
-});
+	//count 숫자 count-up
+	// $('.count .rate li .num strong').counterUp({
+	// 	delay: 10,   /* 애니메이션이 작동되기 이전 대기 시간 */
+	// 	time: 2000    /* 전체 애니메이션 시간 */
+	// });//countup
+
+});//document.ready
