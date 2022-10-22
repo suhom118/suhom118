3385a0930ea5087ec6f4128f6a74b099a115b866
suhom118 <suhom118@gmail.com>
2022-10-22 15:34:31 +0900

    ghj

--- aaa/webapp-master/webapp-master/app/src/main/java/com/hitouch/wikibook/HttpUrl.java
+++
@@ -1,6 +0,0 @@
-package com.hitouch.wikibook;
-
-public class HttpUrl {
-    public static final String WEBURL = "http://wikidocs.net/22308";
-	//public static final String WEBURL = "http://www.naver.com";								 
-}
