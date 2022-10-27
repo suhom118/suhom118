d70978c28f273f0051181851b60c4f7cf823895a
suhom118 <suhom118@gmail.com>
2022-10-27 18:00:42 +0900

    iopiop

--- b&tailor/js/main.js
+++ b&tailor/js/main.js
@@ -32,4 +32,31 @@ $(document).ready(function () {
       $(".visual .popup .btn_wrap .btn_stop").addClass("stop");
     }
   });
+  let scrolling;
+  let moveTop;
+  let objName = $(".fabric .inner");
+  fabScroll();
+  fabScroll2();
+  $(window).scroll(function () {
+    //스크롤할때마다 실행
+    fabScroll();
+    fabScroll2();
+  });
+  function fabScroll() {
+    //스크롤 값을 계산해서 fabric의 이미지를 움직일 함수
+    //스크롤값을 요소의 위치값으로 변경해서 스타일을 적용
+    //요소가 화면의 하단에 등장했을때부터 스크롤 한값을 새로계산
+    scrolling = $(window).scrollTop();
+    objName.position().top;
+    console.log(objName.position().top);
+    moveTop = scrolling * 0.2;
+    objName.css("transform", "translate(0,-" + moveTop + "px)");
+  }
+  function fabScroll2() {
+    //스크롤 값을 계산해서 fabric의 이미지를 움직일 함수
+    scrolling = $(window).scrollTop();
+
+    moveTop = scrolling * 0.1;
+    $(".fabric .bg2 img").css("transform", "translate(0,-" + moveTop + "px)");
+  }
 });
