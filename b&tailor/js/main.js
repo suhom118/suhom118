c7f840a01d9b0b5698c105a53d70c08ef56fce02
suhom118 <suhom118@gmail.com>
2022-10-28 13:10:40 +0900

    rtyrt

--- b&tailor/js/main.js
+++ b&tailor/js/main.js
@@ -31,32 +31,43 @@ $(document).ready(function () {
       swiper.autoplay.stop();
       $(".visual .popup .btn_wrap .btn_stop").addClass("stop");
     }
-  });
-  let scrolling;
-  let moveTop;
-  let objName = $(".fabric .inner");
-  fabScroll();
-  fabScroll2();
-  $(window).scroll(function () {
-    //스크롤할때마다 실행
-    fabScroll();
-    fabScroll2();
-  });
-  function fabScroll() {
-    //스크롤 값을 계산해서 fabric의 이미지를 움직일 함수
-    //스크롤값을 요소의 위치값으로 변경해서 스타일을 적용
-    //요소가 화면의 하단에 등장했을때부터 스크롤 한값을 새로계산
-    scrolling = $(window).scrollTop();
-    objName.position().top;
-    console.log(objName.position().top);
-    moveTop = scrolling * 0.2;
-    objName.css("transform", "translate(0,-" + moveTop + "px)");
-  }
-  function fabScroll2() {
-    //스크롤 값을 계산해서 fabric의 이미지를 움직일 함수
-    scrolling = $(window).scrollTop();
+  }); //visual stop
 
-    moveTop = scrolling * 0.1;
-    $(".fabric .bg2 img").css("transform", "translate(0,-" + moveTop + "px)");
+  //이미지가 스크롤될때 오브젝트가 움직이는 효과
+  //움직이는 시작을 오브젝트가 화면에 나타나기 시작했을때부터 스크롤된값을 계산해서
+  //움직일 값으로 변환해줘야함
+  //1.브라우저가 스크롤되는값-  $(window).scrollTop()
+  //2.오브젝트가 화면 하단에서 나타나기 시작하는 값--offset().top - window().height두값의 차이가 브라우저의 높이값
+  //3.오브젝트를 어떻게 움직일 방법-css로 transform:translate()
+
+  let offTop;
+  let scrolling;
+  let winH;
+  let objMove = $(".fabric .bg1 img"); //실제 움직일 오브젝트
+
+  let objParent = $(".fabric .bg1"); //움직일 오브젝트의 기준이 되는요소
+  let moveVal; //오브젝트가 움직일값
+  let moveDir = "left"; //스크롤 방향(left or top)
+  let moveRate = 0.1; //움직일 속도
+  objMove.css("transition", "0.5s");
+
+  objScroll();
+  $(window).scroll(function () {
+    objScroll();
+  });
+  function objScroll() {
+    scrolling = $(window).scrollTop();
+    // console.log(offTop, "offTop");
+    // console.log(scrolling, "scrolling");
+    // console.log(winH, "winH");
+    winH = $(window).height();
+    offTop = objParent.offset().top;
+    moveVal = (scrolling - offTop + winH) * moveRate;
+    console.log(moveVal, "moveVal");
+    objMove.css("transform", "translate(0,-" + moveVal + "px)");
+
+    moveVal = scrolling - offTop + winH;
+    objMove = $(".fabric .bg2");
+    objMove.css("transform", "translate(0,-" + moveVal + "px)");
   }
 });
